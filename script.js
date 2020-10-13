fdb286a452d1b65adec6d6e6d4ddcd33724fccb3
Motunstech <72519430+Motunstech@users.noreply.github.com>
2020-10-13 11:51:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function hamburger(){
+   /* var togg = document.getElementById('hamb');
+    togg.style.backgroundColor = 'red';*/
+    /*document.getElementById('hamb1').style.backgroundColor = 'red';
+    document.getElementById('hamb2').style.backgroundColor = 'red';
+    document.getElementById('hamb3').style.backgroundColor = 'red';*/
+    document.getElementById('hamburger').classList.toggle('toggle_new');
+    document.getElementById('section').classList.toggle('display');
+}
+function myFunction1()  {
+    var x = document.getElementById("atswa");
+    if (x.style.display === "none") {
+        x.style.display = "block" ;
+    }
+    else {
+        x.style.display = "none" ;
+    }
+}
+function myFunction()  {
+    var y = document.getElementById("form");
+    if (y.style.display === "none") {
+        y.style.display = "block" ;
+    }
+    else {
+        y.style.display = "none" ;
+    }
+}
